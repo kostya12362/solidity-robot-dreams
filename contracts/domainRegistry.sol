f3c934c57fb95200ec7913ea1db4adeddf453045
kostya12362 <rota199804@gmail.com>
2023-09-26 12:59:52 +0300

    Add validation top level domain name

--- contracts/domainRegistry.sol
+++ contracts/domainRegistry.sol
@@ -2,10 +2,11 @@
 pragma solidity ^0.8.0;
 
 contract DomainRegistry {
+    uint256 public collateral  = 0.001 ether;
+
     struct Domain {
         address owner;
         uint256 deposit;
-        bool isRegistered;
     }
 
     mapping(string => Domain) public domains;
@@ -13,27 +14,58 @@ contract DomainRegistry {
     event DomainRegistered(string domainName, address indexed owner, uint256 deposit);
     event DomainReleased(string domainName, address indexed owner, uint256 deposit);
 
+    function toLower(string memory _str) internal pure returns (string memory) {
+        // convert sting to lover case
+        bytes memory bStr = bytes(_str);
+        bytes memory bLower = new bytes(bStr.length);
+        for (uint i = 0; i < bStr.length; i++) {
+            // Uppercase character
+            if ((uint8(bStr[i]) >= 65) && (uint8(bStr[i]) <= 90)) {
+                bLower[i] = bytes1(uint8(bStr[i]) + 32);
+            } else {
+                bLower[i] = bStr[i];
+            }
+        }
+        return string(bLower);
+    }
+
+    function containsSpecialChars(string memory input) internal pure returns (bool) {
+        bytes memory inputBytes = bytes(input);
+        for (uint i = 0; i < inputBytes.length; i++) {
+            uint8 charCode = uint8(inputBytes[i]);
+            if ((charCode < 97 || charCode > 122)) {
+                return true; // A character has been found that does not have a letter [a-z] or a dot
+            }
+        }
+        return false; // All symbols are letters [a-z] or specks
+    }
+
+    modifier checkDomainNameToLowerCase(string memory domainName) {
+        require(keccak256(abi.encodePacked(domainName)) == keccak256(abi.encodePacked(toLower(domainName))), "Domain name must be in lower case");
+        require(containsSpecialChars(domainName) == false, "It is prohibited to use special characters");
+        _;
+    }
+
     modifier domainNotRegistered(string memory domainName) {
-        require(!domains[domainName].isRegistered, "Domain is already registered");
+        require(domains[domainName].owner == address(0), "Domain is already registered");
         _;
     }
 
     modifier domainRegistered(string memory domainName) {
-        require(domains[domainName].isRegistered, "Domain is not registered");
+        require(domains[domainName].owner != address(0), "Domain is not registered");
         _;
     }
 
-    function registerDomain(string memory domainName) external payable domainNotRegistered(domainName) {
-        require(msg.value > 0, "Deposit must be greater than 0 ETH");
+    function registerDomain(string memory domainName) external payable domainNotRegistered(domainName) checkDomainNameToLowerCase(domainName) {
+        require(msg.value == collateral, "Deposit must equal to collateral");
         domains[domainName] = Domain({
             owner: msg.sender,
-            deposit: msg.value,
-            isRegistered: true
+            deposit: msg.value
         });
         emit DomainRegistered(domainName, msg.sender, msg.value);
     }
 
-    function releaseDomain(string memory domainName) external domainRegistered(domainName) {
+    function releaseDomain(string memory domainName) external domainRegistered(domainName) checkDomainNameToLowerCase(domainName) {
         require(msg.sender == domains[domainName].owner, "Only the owner can release the domain");
         uint256 deposit = domains[domainName].deposit;
         payable(msg.sender).transfer(deposit);
--- test/domainRegistry.test.js
+++ test/domainRegistry.test.js
@@ -6,7 +6,6 @@ const { ethers } = require("hardhat");
 describe("DomainRegistry", function () {
   let DomainRegistry;
   let domainRegistry;
-  let owner;
   let addr1;
   let addr2;
 
@@ -16,9 +15,52 @@ describe("DomainRegistry", function () {
     domainRegistry = await DomainRegistry.deploy();
   });
 
+  it("Should top level domain special characters", async function () {
+    const invalidDomainNames = [".com", "com.", "domain@name", "special*char", "example.com"];
+
+    const depositAmount = ethers.parseEther("0.001");
+    for (const domainName of invalidDomainNames) {
+      await expect(
+        domainRegistry
+          .connect(addr1)
+          .registerDomain(domainName, { value: depositAmount })
+      ).to.be.revertedWith("It is prohibited to use special characters");
+    }
+  });
+
+  it("Should with upercase", async function () {
+    const invalidDomainNames = [
+      "UA",
+      "Com",
+      "eXample",
+    ];
+
+    const depositAmount = ethers.parseEther("0.001");
+    for (const domainName of invalidDomainNames) {
+      await expect(
+        domainRegistry
+          .connect(addr1)
+          .registerDomain(domainName, { value: depositAmount })
+      ).to.be.revertedWith("Domain name must be in lower case");
+    }
+  });
+
+  it("Should top level domain special characters", async function () {
+    const invalidDomainNames = [".com", "com.", "domain@name", "special*char"];
+
+    const depositAmount = ethers.parseEther("0.001");
+    for (const domainName of invalidDomainNames) {
+      await expect(
+        domainRegistry
+          .connect(addr1)
+          .registerDomain(domainName, { value: depositAmount })
+      ).to.be.revertedWith("It is prohibited to use special characters");
+    }
+  });
+
   it("Should register a domain with a deposit", async function () {
-    const domainName = "example.com";
-    const depositAmount = ethers.parseEther("1");
+    const domainName = "example";
+    const depositAmount = ethers.parseEther("0.001");
     await domainRegistry
       .connect(addr1)
       .registerDomain(domainName, { value: depositAmount });
@@ -26,12 +68,11 @@ describe("DomainRegistry", function () {
     const domain = await domainRegistry.domains(domainName);
     expect(domain.owner).to.equal(addr1.address);
     expect(domain.deposit).to.equal(depositAmount);
-    expect(domain.isRegistered).to.equal(true);
   });
 
   it("Should release a domain and refund the deposit", async function () {
-     const domainName = "example.com";
-    const depositAmount = ethers.parseEther("1");
+    const domainName = "example";
+    const depositAmount = ethers.parseEther("0.001");
 
     await domainRegistry
       .connect(addr1)
@@ -39,29 +80,27 @@ describe("DomainRegistry", function () {
     const balanceBefore = await ethers.provider.getBalance(addr1);
     const tx = await domainRegistry.connect(addr1).releaseDomain(domainName);
     const receipt = await tx.wait(); // Очікуємо на підтвердження транзакції
-    // console.log(receipt)
     const gasUsed = receipt.gasUsed * tx.gasPrice; // Газові витрати * ціна газу
     const balanceAfter = await ethers.provider.getBalance(addr1);
     const domain = await domainRegistry.domains(domainName);
 
     expect(domain.owner).to.equal("0x0000000000000000000000000000000000000000");
     expect(domain.deposit).to.equal(0n);
-    expect(domain.isRegistered).to.equal(false);
 
-    // Перевірка, що баланс зменшився на depositAmount + газові витрати
+    // Check that the balance has changed to depositAmount + gas costs
     const expectedBalanceChange = depositAmount - gasUsed;
     expect(balanceAfter - balanceBefore).to.equal(expectedBalanceChange);
   });
 
   it("Should not allow registration of an already registered domain", async function () {
-    const domainName = "example.com";
-    const depositAmount = ethers.parseEther("1");
+    const domainName = "gov";
+    const depositAmount = ethers.parseEther("0.001");
 
     await domainRegistry
       .connect(addr1)
       .registerDomain(domainName, { value: depositAmount });
 
-    // Спроба зареєструвати той же домен вдруге
+    // Try to register the same domain suddenly
     await expect(
       domainRegistry
         .connect(addr2)
@@ -70,25 +109,35 @@ describe("DomainRegistry", function () {
   });
 
   it("Should not allow release of a domain by a non-owner", async function () {
-    const domainName = "example.com";
-    const depositAmount = ethers.parseEther("1");
+    const domainName = "example";
+    const depositAmount = ethers.parseEther("0.001");
 
     await domainRegistry
       .connect(addr1)
       .registerDomain(domainName, { value: depositAmount });
 
-    // Спроба звільнити домен не власником
+    // An attempt to release a domain that is not the owner
     await expect(
       domainRegistry.connect(addr2).releaseDomain(domainName)
     ).to.be.revertedWith("Only the owner can release the domain");
   });
 
   it("Should not allow release of an unregistered domain", async function () {
-    const domainName = "example.com";
-
-    // Спроба звільнити незареєстрований домен
+    const domainName = "com";
+    // Attempting to release an unregistered domain
     await expect(
       domainRegistry.connect(addr1).releaseDomain(domainName)
     ).to.be.revertedWith("Domain is not registered");
+  });
+  it("Transfer of invalid deposit", async function () {
+    const domainName = "ua";
+    const depositAmount = ethers.parseEther("1");
+    const defaulDeposit = await domainRegistry.collateral();
+    expect(defaulDeposit).to.not.equal(depositAmount);
+    await expect(
+      domainRegistry
+        .connect(addr1)
+        .registerDomain(domainName, { value: depositAmount })
+    ).to.be.revertedWith("Deposit must equal to collateral");
   });
 });
